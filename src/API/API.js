6f9a93ad7b6219ead68e82ea2a78022795798a4d
Загидуллин Р.Н <r.zagidullin@ios.icl>
2019-12-23 13:20:08 +0300

    API added

---
+++ src/API/API.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://front-test.beta.aviasales.ru/',
+});
---
+++ src/API/index.js
@@ -0,0 +1,3 @@
+import axios from './API';
+
+export default axios;
