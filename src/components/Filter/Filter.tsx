f4af49acc1ea78f4f39dcd2cdaf3e3e0194f15d5
Bob_chemist <bob_chemist@mail.ru>
2019-12-24 23:23:56 +0300

    Filter component refactored

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import classes from './Filter.module.sass';
 
 interface IFilter {
+  [key: string]: boolean,
   'all': boolean,
   '0': boolean,
   '1': boolean,
   '2': boolean,
   '3': boolean,
-  [key: string]: boolean,
+}
+
+interface ILanding {
+  [key: string]: string
 }
 
 const Filter = () => {
-
-  const [ filter, setFilter ] = useState({
+  const [filter, setFilter] = useState<IFilter>({
     'all': true,
     '0': true,
     '1': true,
@@ -56,37 +59,44 @@ const Filter = () => {
     setFilter(newFilter);
   };
 
-  const { filterBlock, filterBlockTitle, filterBlockList, filterBlockListitem } = classes;
+  const landings = ['all', '0', '1', '2', '3'];
+
+  const landingsArray: ILanding = {
+    'all': 'Все',
+    '0': 'Без пересадок',
+    '1': '1 пересадка',
+    '2': '2 пересадки',
+    '3': '3 пересадки',
+  };
+
+  const renderLabel = (key: string) => {
+    const bool = filter[key];
+
+    return (
+      <label className={filterBlockListitem}>
+        <input type='checkbox' checked={bool} value={key} onChange={filterChange} />
+        <div className={filterBlockInput} style={bool ? undefined : { borderColor: '#9ABBCE' } }>
+          <div className={filterBlockInputCheck} style={bool ? { display: 'block' } : undefined}></div>
+        </div>
+        {landingsArray[key]}
+      </label>
+    );
+  };
+
+  const {
+    filterBlock,
+    filterBlockTitle,
+    filterBlockList,
+    filterBlockListitem,
+    filterBlockInput,
+    filterBlockInputCheck,
+  } = classes;
 
   return (
     <div className={filterBlock}>
       <h3 className={filterBlockTitle}>Количество пересадок</h3>
-
       <div className={filterBlockList}>
-        <label className={filterBlockListitem}>
-          <input type='checkbox' checked={filter['all']} value={'all'} onChange={filterChange} />
-          Все
-        </label>
-
-        <label className={filterBlockListitem}>
-          <input type='checkbox' checked={filter['0']} value={'0'} onChange={filterChange} />
-          Без пересадок
-        </label>
-
-        <label className={filterBlockListitem}>
-          <input type='checkbox' checked={filter['1']} value={'1'} onChange={filterChange} />
-          1 пересадка
-        </label>
-
-        <label className={filterBlockListitem}>
-          <input type='checkbox' checked={filter['2']} value={'2'} onChange={filterChange} />
-          2 пересадки
-        </label>
-
-        <label className={filterBlockListitem}>
-          <input type='checkbox' checked={filter[3]} value={3} onChange={filterChange} />
-          3 пересадки
-        </label>
+        {landings.map(renderLabel)}
       </div>
     </div>
   );
